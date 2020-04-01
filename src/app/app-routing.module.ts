c0dbddfa79ea92304e3e54d6dac6dc5a804c5a3f
regina fitriani <regina1800016055@webmail.uad.ac.id>
2020-04-01 22:46:21 +0800

    add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { StudentsComponent } from './pages/students/students.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+	path:'home',
+	component:HomeComponent
+},
+{
+	path:'teacher',
+	component:TeacherComponent
+},
+{
+	path:'students',
+	component:StudentsComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { StudentsComponent } from './pages/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeacherComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
